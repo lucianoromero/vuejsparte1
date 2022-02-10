a62ee0064cc245e3a0eb5472b9115574e8d0263c
Luciano Romero <luciano.romero@vmtecnologia.io>
2022-02-10 17:31:50 -0300

    06 Animando transições de pagina  - Aplicando efeito de transição entre as paginas

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="corpo">
-    
-   <nav>
-      <ul>       
-          <li v-for="route in routes" :key="route.titulo">
-            <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link>
-          </li>
+  
+    <nav>
+      <ul>
+        <li v-for="route in routes" :key="route.titulo">
+          <router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link>
+        </li>
       </ul>
     </nav>
 
-    <router-view></router-view>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import { routes }  from './routes';
+import { routes } from './routes';
 export default {
   data() {
     return {
@@ -30,5 +32,14 @@ export default {
   font-family: Helvetica, sans-serif;
   margin: 0 auto;
   width: 96%;
+}
+
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.3s;
+}
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
 }
 </style>
