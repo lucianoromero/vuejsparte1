869fabef7740076df98a07a7986ddec7ae981c14
Luciano Romero <luciano.romero@vmtecnologia.io>
2022-02-10 17:52:39 -0300

    06 Transformando o menu em componente

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="corpo">
-  
-    <nav>
-      <ul>
-        <li v-for="route in routes" :key="route.titulo">
-          <router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link>
-        </li>
-      </ul>
-    </nav>
+    <meu-menu :rotas="routes" />
 
     <transition name="pagina">
       <router-view></router-view>
@@ -17,7 +10,13 @@
 
 <script>
 import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
+
 export default {
+  components: {
+    'meu-menu': Menu,
+  },
+
   data() {
     return {
       routes,
